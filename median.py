5ac72c26b6ee5f76aab78f496c0b3a05f322bc70
Ricky Brown <ricky.brown@kcl.ac.uk>
2022-10-03 12:01:54 +0100

    Update median.py

--- median.py
+++ median.py
@@ -1,12 +1,27 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+# while True:
+#     try:
+#         print("Enter a list of numbers separated by commas: ")
+#         numbers = [float(value) for value in input().split(",")]
+#     except ValueError:
+#         print("Some input could not be converted to a number!")
+#     else:
+#         break
+# print(numbers)
+medianValue = 0
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        median = len(numbers)//2
+        medianNo = numbers[median]
+
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print("median: " + str(medianNo))
